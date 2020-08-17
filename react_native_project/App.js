209074dbfa1f1cac6392c3f0fde64b864b157925
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-08-18 08:33:56 +0900

    add react native project

--- react_native_project/App.js
+++ react_native_project/App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="JaneProfile" component={JaneProfileScreen} />
+        <Stack.Screen name="BobProfile" component={BobProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to jane's profile"
+      onPress={() =>
+        navigation.push('JaneProfile', { name: 'Jane' })
+      }
+    />
+  );
+};
+
+const JaneProfileScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>This is Jane's profile</Text>
+      <Button
+        title="Go to Bob's profile"
+        onPress={() =>
+          navigation.push('BobProfile', { name: 'Bob' })
+        }
+      />
+    </View>
+  );
+};
+
+const BobProfileScreen = () => {
+  return <Text>This is Bob's profile</Text>;
+};
