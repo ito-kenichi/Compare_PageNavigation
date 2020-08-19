2d4ddc19c91df2d643e36a04e5e1c5ebba95bf29
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-08-19 20:41:19 +0900

    add xamarin.forms

---
+++ xamarin.forms_project/xamarin.forms_project/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace xamarin.forms_project.Models
+{
+    public class Note
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
